ecd2859d4fbb81957b1495a973ab8c5950802053
ahmed samir <ahmed.samir.farid@gmail.com>
2024-02-24 17:28:07 +0200

    V 0.1

---
+++ components/Section/Hero.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts"></script>
+<template>
+  <section
+    id="hero"
+    class="relative md:h-screen grid content-center dark:bg-[url('/img/BG.svg')] py-36"
+  >
+    <div
+      class="py-8 px-4 mx-auto max-w-screen-xl text-center relative z-10 lg:py-16 lg:px-12 self-center"
+    >
+      <h1
+        class="flex justify-center items-center mb-4 text-4xl md:text-5xl lg:text-7xl font-extrabold tracking-tight leading-none text-gray-800 dark:text-white relative"
+      >
+        Buy, Create & Sell Unique NFTs File.
+      </h1>
+
+      <p
+        class="mb-8 m-6 text-lg font-medium text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400"
+      >
+      NFTs are used for digital art and collectibles, GameFi projects, metaverses, and more.
+      </p>
+      <div
+        class="flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4"
+      >
+        <button
+          type="button"
+          class="inline-flex items-center text-sm font-semibold justify-center py-3 px-5 text-center rounded-lg text-white bg-gradient-to-b from-[#DF1CFF] to-[#EA0070] shadow-[0px 9px 100px 0px rgb(208 17 204 / 35%)]"
+        >
+        Explore NFTs
+        </button>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ components/layout/AppFooter.vue
@@ -0,0 +1,58 @@
+<script setup>
+const SocialData = ref([
+  {
+    image: "email",
+    label: "email",
+  },
+  {
+    image: "instagram",
+    label: "instagram",
+  },
+  {
+    image: "twitter",
+    label: "twitter",
+  },
+  {
+    image: "facebook",
+    label: "facebook",
+  },
+  {
+    image: "linkedin",
+    label: "linkedin",
+  },
+]);
+</script>
+
+<template>
+  <footer class="bg-gradient-to-r from-[#FFD600] to-[#FFB800]">
+    <div class="py-[25px] px-0 border-t border-slate-400">
+      <div class="container mx-auto relative">
+        <div class="flex justify-around">
+          <!-- Grid -->
+          <div class="grid grid-cols-1 md:grid-cols-3 items-center gap-5 text-center">
+
+            <p class="mb-0 text-sm font-semibold text-dark">
+              Copyright © 2022 Nazmul Shanto. All Rights Reserved
+            </p>
+
+            <!-- Social Brands -->
+            <div class="md:text-end">
+          
+              <NuxtLink
+              v-for="(social, index) in SocialData" :key="index"
+                target="_blank"
+                class="h-8 w-8 mx-1 cursor-pointer inline-flex items-center justify-center tracking-wide align-middle duration-500 text-[15px] text-center border border-gray-800 dark:border-slate-800 rounded-xl hover:border-[#f59e20] dark:hover:border-[#f59e20] hover:bg-[#f59e20] dark:hover:bg-[#f59e20] text-slate-300 hover:text-white"
+              >
+              <img class="opacity-70" :src="`/img/svg/${social.image}.svg`"/>
+          <!--       <img class="opacity-70" :src="require(`/img/svg/${social.image}`)" /> -->
+              </NuxtLink>
+  
+            <!-- End Social Brands -->
+          </div>
+          <!-- End Grid -->
+        </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
